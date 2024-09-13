5ad0aded3ab133ac47aa8770839dd970f0098e8b
“justino1806” <justino.dosreis4@gmail.com>
2024-09-13 16:21:58 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -56,10 +56,26 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas" + realizadas.length,
         choices: [...realizadas]
     })
 
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((metas) => {
+        return meta.checked != true
+    })
+
+    if (abertas.length == 0) {
+        console.log("Não existem metas abertas")
+        return
+    }
+
+    await select({
+        message: "Metas Abertas" = abertas.length,
+        choices: [...abertas]
+    })
 }
 
 const start = async() => {
@@ -81,6 +97,10 @@ const start = async() => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 },
@@ -97,6 +117,9 @@ const start = async() => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a proxima")
