89e23d6d5e0a10ec4e2e9c226cb2bc417b6df259
“justino1806” <justino.dosreis4@gmail.com>
2024-09-13 15:05:43 -0300

    menu da aplicação

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const start = () => {
+const {select} = require('@inquirer/prompts')
+
+const start = async() => {
     let count = 0
     while (true) {
-        let opcao = "cadastrar"
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                },
+            ]
+        })
+
         switch (opcao) {
             case "cadastrar":
                 console.log("Vamos cadastrar")
                 break;
             case "listar":
                 console.log("Vamos listar")
+                break
             case "sair":
+                console.log("Até a proxima")
                 return
         }
     }
